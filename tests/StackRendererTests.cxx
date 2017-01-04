7ff7f01ecf811e0fb21fc4d6216bc0e9c734008a
Onat Bas <i@onat.me>
2017-01-04 02:33:12 +0200

    Added Stack renderer tests.

---
+++ tests/StackRendererTests.cxx
@@ -0,0 +1,31 @@
+//
+// Created by Onat Bas on 04/01/17.
+//
+
+#include "gtest/gtest.h"
+
+
+TEST(StackRendererTests, shouldDisplayStack)
+{
+
+}
+
+TEST(StackRendererTests, shouldDisplayStackCorrectColors)
+{
+
+}
+
+TEST(StackRendererTests, shouldDisplayStackOnPlace)
+{
+
+}
+
+TEST(StackRendererTests, shouldDisplayStackSetWithCorrectColors)
+{
+
+}
+
+TEST(StackRendererTests, shouldDisplayStackSetOnPlace)
+{
+
+}
